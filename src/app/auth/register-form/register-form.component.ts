865247332fb0d90c8b9b37f87fcba97bf2f8db6c
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2023-11-11 15:46:34 +0100

    add register page

--- src/app/auth/register-form/register-form.component.ts
+++ src/app/auth/register-form/register-form.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.css'
+  styleUrl: './register-form.component.css',
 })
 export class RegisterFormComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirm_password: ['', Validators.required],
+    });
+  }
 }
